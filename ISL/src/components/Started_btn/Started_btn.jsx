ae929b098dabeea46cf1cb4b621d07d12fc92943
BalkrishanSingh <Balkrishan62005@gmail.com>
2024-10-11 11:48:18 +0530

    Changed design for startup page.

--- ISL/src/components/Started_btn/Started_btn.jsx
+++ ISL/src/components/Started_btn/Started_btn.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Started_btn.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Started_btn.css";
 
 const Started_btn = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('/next-page'); // Replace '/next-page' with your desired route
+    navigate("/next-page"); // Replace '/next-page' with your desired route
   };
 
   return (
     <button
+      className="bg-blue-500 text-white font-bold py-4 px-8 rounded-full shadow-lg transform transition duration-500 hover:bg-blue-400 hover:scale-110 hover:shadow-2xl"
       onClick={handleClick}
-      className="start_para"
     >
       Get Started
     </button>
